33e32e3dacab1f3b90e249d9652602a48901cf55
antoniogrecoo <queirozgreco@gmail.com>
2025-01-03 20:29:09 -0400

    segundo commit

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function alterarTexto() {
     var texto = document.querySelector('.texto1');  // Seleciona o elemento com a classe texto1
     var texto2 = document.querySelector('.texto2');
     var texto3 = document.querySelector('.texto-especialista');
+    var texto4 = document.querySelector('.txt-assessoria');
     if (window.innerWidth < 768) {
         texto.innerHTML = "A ASSESSORIA JURÍDICA<br>QUE<b> SEU CONDOMÍNIO<br>PRECISA!</b>";  // Altera o texto para "TESTE/TESTE" em telas menores que 768px
         texto2.innerHTML = "A Importância da Assessoria<BR>Jurídica nos Condomínios";
         texto3.innerHTML = "FALE COM UM ESPECIALISTA";
+        texto4.innerHTML = "COMO FUNCIONA <b>NOSSA ASSESSORIA?</b>";
     } else {
         texto.innerHTML = "A ASSESSORIA JURÍDICA QUE<br><b>SEU CONDOMÍNIO PRECISA!</b>";  // Retorna o texto original
         texto2.innerHTML = "A Importância da Assessoria Jurídica nos Condomínios";
-        texto3.innerHTML = "FALE COM UM ESPECIALISTA ";
+        texto3.innerHTML = "FALE COM UM ESPECIALISTA";
+        texto4.innerHTML = "COMO FUNCIONA <b>NOSSA<br> ASSESSORIA?</b>";
     }
 }
 
